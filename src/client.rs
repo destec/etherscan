00085d71f5ef9ed6f7d976161d902097830ac3b8
destec <destecx@gmail.com>
2018-06-02 11:56:28 +0800

    try to add deserializor for response result

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,8 @@
 use reqwest;
+use reqwest::{Response, StatusCode};
+use serde::{Deserialize};
+use serde_json;
+use std::io::Error;
 
 static API_HOST: &'static str = "https://api.etherscan.io/api";
 
@@ -12,11 +16,30 @@ impl Client {
     Client { api_key: api_key }
   }
 
-  pub fn get(&self, endpoint: String) -> Result<reqwest::Response, reqwest::Error> {
+  pub fn get<T>(&self, endpoint: String) -> T where T: Into<Deserialize>{
     let mut url = format!("{}?{}", API_HOST, endpoint);
     url.push_str(format!("&apikey={}", self.api_key).as_str());
-    let client = reqwest::Client::new();
-    let response = client.get(url.as_str()).send();
-    return response;
+    let response = reqwest::get(url.as_str()).unwrap();
+    let result = self.process(response).unwrap();
+    let object: T = serde_json::from_str(&result).unwrap();
+    return object;
+  }
+
+  fn process(&self, response: Response) -> Result<String, Error> {
+    match response.status() {
+      StatusCode::Ok => {
+        let body = response.text();
+        match body {
+          Ok(result) => return Ok(result),
+          // Err(e) => Error::from_str(format!("{:?}", e)),
+        }
+      }
+      StatusCode::RequestTimeout => {
+        return Ok(format!("{}", "timeout"));
+      }
+      _ => {
+        return Ok(format!("{:?}", response));
+      }
+    }
   }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
 
 pub mod client;
